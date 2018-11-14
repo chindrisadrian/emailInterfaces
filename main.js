2424e1b67c1773130d28fddcfb013c4f482c624b
Adrian Chindris <adrian.chindris@syneto.eu>
2018-11-14 17:42:11 +0200

    4

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 	$('a:not(".title")').click(function (event) {
 		$(event.currentTarget).addClass('active').siblings().removeClass('active');
 	});
+	
+	$('.item:not(".title")').click(function (event) {
+		$(event.currentTarget).addClass('active').siblings().removeClass('active');
+	});
